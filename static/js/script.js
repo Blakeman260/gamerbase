9ca53d1aa93070c2a190cac0d92fbbfabefbcc8a
Blakeman260 <Andy_blakeman@hotmail.co.uk>
2020-11-21 22:14:26 +0000

    added colors to base.html, editted nav bar, for better suited location for buttons

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $('.sidenav').sidenav({edge: "right"});
+    $('.sidenav').sidenav();
     $('.collapsible').collapsible();
     $(".dropdown-trigger").dropdown();
 });
