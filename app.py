fa614875ea741e24a4b2ec9857b96a415498c016
Blakeman260 <Andy_blakeman@hotmail.co.uk>
2020-11-23 12:53:24 +0000

    added registration functionality

--- app.py
+++ app.py
@@ -27,6 +27,24 @@ def get_reviews():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        # To check if the username already exists in the database
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("The Username you chose, already exists! :o ")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # This is to put new user into the 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Player registered! ☺")
     return render_template("register.html")
 
 
