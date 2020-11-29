3e40864dec57042136162aa575a3ed5f348ddedf
Blakeman260 <Andy_blakeman@hotmail.co.uk>
2020-11-29 01:33:09 +0000

    connected delete button for users reviews

--- app.py
+++ app.py
@@ -137,6 +137,13 @@ def edit_review(review_id):
         "edit_review.html", review=review, consoles=consoles, recommend=recommend)
 
 
+@app.route("/delete_review/<review_id>")
+def delete_review(review_id):
+    mongo.db.reviews.remove({"_id": ObjectId(review_id)})
+    flash("Your Review was Sucessfully Removed")
+    return redirect(url_for("get_reviews"))
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
     port=int(os.environ.get("PORT")),
